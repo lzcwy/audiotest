3b7c27f7d61f7b9d285275d1c1ddf2d5b01581f5
lzcwy <qqsvip666@163.com>
2023-07-24 17:46:39 +0800

    fix

--- tts.py
+++ tts.py
@@ -31,73 +31,11 @@ class Tts(Plugin):
     def reply(self) -> Reply:
         reply = Reply(ReplyType.TEXT, "Failed to get porn videos")
         try:
-            flag = self.tts_ws()
-            if flag ==True:
+            
+                respone = requests.get('http://127.0.0.1:8000/tts?text=测试游戏阿拉&speaker=BV426_streaming')
+                print(respone)
                 reply = Reply(ReplyType.MP3, "mp3")
         except Exception as e:
             logger.error(f"Audio api call error: {e}")
         return reply
     
-    async def tts_ws(self):
-        result_path = "./output.mp3"
-        payload = {
-            "audio_config": {
-                "bit_rate": 64000,
-                "enable_split": False,
-                "enable_timestamp": False,
-                "format": "mp3",
-                "sample_rate": 24000,
-                "speech_rate": 0
-            },
-            "speaker": "BV426_streaming",
-            "text": "测试一下啦"
-        }
-        api_url = "ws://sami.bytedance.com/internal/api/v2/ws?device_id=2412774897234120&iid=3732188851614814"
-        req = {
-            "appkey": "IZjhUeAYwP",
-            "event": "StartTask",
-            "namespace": "TTS",
-            "payload": json.dumps(payload)
-        }
-        # try:
-        st = time.perf_counter()
-        flag = 0
-        result_data = open(result_path, "wb+")
-        async with websockets.connect(api_url, ping_interval=None) as ws:
-            # 先发送开始事件
-            await ws.send(json.dumps(req))
-            # 然后发送该事件是否发送完成
-            req["event"] = "FinishTask"
-            first_package_time = None
-            await ws.send(json.dumps(req))
-            while True:
-                res = await ws.recv()
-                try:
-                    if isinstance(res, str):
-                        res_dict = json.loads(res)
-                        if "data" in res_dict:
-                            if flag == 0:
-                                first_package_time = time.perf_counter() - st
-                                flag = 1
-                            result_data.write(base64.b64decode(res_dict["data"]))
-                        if "payload" in res_dict:
-                            print(" payload=%s" % res_dict["payload"], end="")
-                        print(" task_id=%s, event=%s status_code=%d status_text=%s" % (
-                            res_dict["task_id"], res_dict["event"], res_dict["status_code"], res_dict["status_text"]))
-                        if res_dict["event"] == "TaskFinished":
-                            # await ws.close()
-                            break
-                    else:
-                        print("receive binary message, len=%d" % len(res))
-                        result_data.write(res)
-                        if flag == 0:
-                            first_package_time = time.perf_counter() - st
-                            flag = 1
-                        # print(res)
-                except Exception as e:
-                    print("exception", e)
-                    break
-            if first_package_time is not None:
-                print("首包时间：", first_package_time)
-        result_data.close()
-        return True
